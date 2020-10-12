713f8c3f113874e166de2b122b5fe4415b9e1016
DD <dd@dd.si>
2020-10-13 00:28:02 +0200

    Some updates .... added error 500 response

--- dd_soap.sql
+++ dd_soap.sql
@@ -2,25 +2,26 @@ create or replace package DD_SOAP is
 
 /*
 STATUS
+13.10.2020 - Updateing package ....
 10.6.2015 - Response is no longer limited ro VARCHAR2 but CLOB
 28.11.2006 - First version - Domen Dolar
 */
 
   C_URL varchar2(100) := 'http://url of target SOAP service...';
-  
-  /*Added 29.6.2017 option to add own header ....*/                                 
+
+  /*Added 29.6.2017 option to add own header ....*/
   type header  IS RECORD (
     name     VARCHAR2(256),
-    value    VARCHAR2(256));                                    
-  
-  type theader is table of header index by binary_integer;    
-                                     
+    value    VARCHAR2(256));
+
+  type theader is table of header index by binary_integer;
+
   /* v.p 04.10.2016 */
-    
+
   C_TIMEOUT number := 180;
-  c_hdr theader; 
-    
-  
+  c_hdr theader;
+
+
   /* A type to represent a SOAP RPC request */
   TYPE request IS RECORD (
     method     VARCHAR2(256),
@@ -30,6 +31,10 @@ STATUS
   /* A type to represent a SOAP RPC response */
   TYPE response IS RECORD (
     doc xmltype);
+
+  function cdata(pXml clob) return clob;
+
+  function ChangeSpecialCode2HTMLUTF(v_text clob) return clob;
 
   /*
    * Create a new SOAP RPC request.
@@ -51,7 +56,7 @@ STATUS
    */
   FUNCTION invoke(req    IN OUT NOCOPY request,
                   url    IN VARCHAR2,
-                  action IN VARCHAR2, 
+                  action IN VARCHAR2,
                   hdr theader default c_hdr
                   ) RETURN response;
 
@@ -61,15 +66,49 @@ STATUS
   FUNCTION get_return_value(resp      IN OUT NOCOPY response,
                             name      IN VARCHAR2,
                             namespace IN VARCHAR2) RETURN clob;
-                            
+
   FUNCTION get_return_value(resp      IN OUT NOCOPY response) RETURN clob;
-                            
+
   PROCEDURE show_envelope(env IN VARCHAR2);
 
 end;
 /
 create or replace package body DD_SOAP is
+
+function cdata(pXml clob) return clob is
+ begin
+ return '<![CDATA['||pXml||']]>';
+ end;
  
+ 
+
+function ChangeSpecialCode2HTMLUTF(v_text clob) return clob is
+  begin
+--https://www.rapidtables.com/code/text/unicode-characters.html    
+return replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(v_text,
+                                                                                'æ',
+                                                                                '&#263;'),
+                                                                        'Æ',
+                                                                        '&#262;'),
+                                                                'ð',
+                                                                '&#273;'),
+                                                        'Ð',
+                                                        '&#272;'),
+                                                'ž',
+                                                '&#382;'),
+                                        'Ž',
+                                        '&#381;'),
+                                'è',
+                                '&#269;'),
+                        'È',
+                        '&#268;'),
+                'š',
+                '&#353;'),
+        'Š',
+        '&#352;');
+end;
+
+
   FUNCTION new_request(method    IN VARCHAR2,
                        namespace IN VARCHAR2)
                        RETURN request AS
@@ -86,9 +125,9 @@ create or replace package body DD_SOAP is
                           value IN CLOB) AS
   BEGIN
        if type is null then
-    req.body := req.body || '<'||name||'>'||value||'</'||name||'>';
+    req.body := req.body || '<'||name||'>'||ChangeSpecialCode2HTMLUTF(value)||'</'||name||'>';
        else  
-    req.body := req.body || '<'||name||' xsi:type="'||type||'">'||value||'</'||name||'>';
+    req.body := req.body || '<'||name||' xsi:type="'||type||'">'||ChangeSpecialCode2HTMLUTF(value)||'</'||name||'>';
        end if;
   END;
 
@@ -96,7 +135,7 @@ create or replace package body DD_SOAP is
             env IN OUT NOCOPY CLOB) AS
   BEGIN
     env := '<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
+    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
                        xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance"
                        xmlns:xsd="http://www.w3.org/1999/XMLSchema"
                        xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/">
@@ -118,7 +157,7 @@ req.body||'</'||req.method||'>
     END LOOP;
   END;
 
-  PROCEDURE check_fault(req request, resp IN OUT NOCOPY response) AS    
+  PROCEDURE check_fault(req request, resp IN OUT NOCOPY response) AS
     fault_node   xmltype;
     fault_code   VARCHAR2(256);
     fault_string VARCHAR2(32767);
@@ -150,15 +189,18 @@ req.body||'</'||req.method||'>
   eob boolean := false;
   requestbody clob;
   v_url varchar2(1000);
+  v_t date;
+  v_i number;  
 begin
+    v_t := sysdate;
     generate_envelope(req, requestbody);
 
   --  show_envelope(requestbody);
 
---return resp;  
+--return resp;
   utl_http.set_transfer_timeout(C_TIMEOUT);
-  
-  
+
+
   if instr(upper(url),'HTTP') = 0 then v_url :=  C_URL||url; else v_url := url; end if;
   http_req := utl_http.begin_request(v_url, 'POST', 'HTTP/1.1');
   utl_http.set_header(http_req, 'Content-Type', 'text/xml; charset=UTF-8');
@@ -167,14 +209,14 @@ begin
                       action);
   for i in 1..hdr.count loop
     utl_http.set_header(http_req, hdr(i).name, hdr(i).value );
-  end loop;                      
- -- utl_http.set_header(http_req, 'Transfer-Encoding', 'chunked'); 
+  end loop;
+ -- utl_http.set_header(http_req, 'Transfer-Encoding', 'chunked');
   reqlength := dbms_lob.getlength(requestbody);
   utl_http.set_header(http_req, 'Content-Length', reqlength );
-  while (offset < reqlength) loop 
-    dbms_lob.read(requestbody, amount, offset, buffer);  
+  while (offset < reqlength) loop
+    dbms_lob.read(requestbody, amount, offset, buffer);
     utl_http.write_text(http_req, buffer);
-    -- dbms_output.put_line('============================================');  
+    -- dbms_output.put_line('============================================');
     -- dbms_output.put_line(buffer);
     offset := offset + amount;
   end loop;
@@ -196,22 +238,28 @@ begin
 --  dbms_output.put_line('After resp loop');
     utl_http.end_response(http_resp);
 
-    if instr(responsebody,'<?xml') > 0 then
-       responsebody := substr(responsebody,instr(upper(responsebody),'<?XML')); 
-       responsebody := substr(responsebody,1, instr(upper(responsebody),'ENVELOPE>')+9 );       
-    end if;  
+    if http_resp.status_code = 500 then
+       v_i := round((sysdate - v_t)*3600*24);
+       raise_application_error('-20000','HTTP ERROR: 500 Internal Server Error; EXECUTION TIME(s):'||v_i);
+    end if;
 
- --dbms_output.put_line(substr(responsebody,1,10000));
+    if instr(responsebody,'<?xml') > 0 then
+       responsebody := substr(responsebody,instr(upper(responsebody),'<?XML'));
+       responsebody := substr(responsebody,1, instr(upper(responsebody),'ENVELOPE>')+9 );
+    end if;
+
+--dbms_output.put_line('responsebody: ' || substr(responsebody,1,10000));
 --return resp;
+
     if length(responsebody) > 1 then
        resp.doc := xmltype.createxml(responsebody);
        resp.doc := resp.doc.extract('/soap:Envelope/soap:Body/child::node()',
          'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"');
 
-        --show_envelope(resp.doc.getstringval());        
-       check_fault(req,resp);       
+        --show_envelope(resp.doc.getstringval());
+       check_fault(req,resp);
     end if;
-    
+
     RETURN resp;
     DBMS_LOB.freetemporary(responsebody);
   END;
@@ -238,11 +286,11 @@ begin
     --show_envelope(http_req.method);
 
     http_resp := utl_http.get_response(http_req);
-    utl_http.read_text(http_resp, env);   
+    utl_http.read_text(http_resp, env);
     utl_http.end_response(http_resp);
-    
+
    -- htp.p(env);
-    
+
     resp.doc := xmltype.createxml(env);
     resp.doc := resp.doc.extract('/soap:Envelope/soap:Body/child::node()',
       'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"');
@@ -260,14 +308,14 @@ begin
     RETURN resp.doc.extract('//'||name||'/child::text()',
       namespace).getclobval();
   exception when others then
-    return '';     
+    return '';
   END;
 
   FUNCTION get_return_value(resp      IN OUT NOCOPY response) RETURN clob AS
   BEGIN
     RETURN resp.doc.getclobval();
   exception when others then
-    return '';     
+    return '';
   END;
 
 
